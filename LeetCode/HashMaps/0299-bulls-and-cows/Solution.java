d59f533ffcdd0788d24c277372d1fdafd655453d
Kartik Krishna Kumbhar <166889480+kartikkumbhar150@users.noreply.github.com>
2026-08-08 23:48:49 +0530

    Solved 299. Bulls and Cows

---
+++ LeetCode/HashMaps/0299-bulls-and-cows/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+
+        int bulls = 0;
+        int cows = 0;
+
+        int[] freqSecret = new int[10];
+        int[] freqGuess = new int[10];
+
+        for (int i = 0; i < secret.length(); i++) {
+
+            if (secret.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else {
+                freqSecret[secret.charAt(i) - '0']++;
+                freqGuess[guess.charAt(i) - '0']++;
+            }
+        }
+
+        
+        for (int i = 0; i < 10; i++) {
+            cows += Math.min(freqSecret[i], freqGuess[i]);
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
