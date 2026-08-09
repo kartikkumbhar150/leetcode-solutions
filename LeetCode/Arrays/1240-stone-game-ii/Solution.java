9ae308e682729d435e56b9a1c630c52f2f7f8303
Kartik Krishna Kumbhar <166889480+kartikkumbhar150@users.noreply.github.com>
2026-08-09 16:08:38 +0530

    Solved 1240. Stone Game II

---
+++ LeetCode/Arrays/1240-stone-game-ii/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+
+        int[] suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int M = n; M >= 1; M--) {
+
+                if (2 * M >= n - i) {
+                    dp[i][M] = suffix[i];
+                    continue;
+                }
+
+                for (int X = 1; X <= 2 * M; X++) {
+                    dp[i][M] = Math.max(
+                        dp[i][M],
+                        suffix[i] - dp[i + X][Math.max(M, X)]
+                    );
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+}
