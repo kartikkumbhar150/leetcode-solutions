70035e8d1df778644c07829c70af5399e25ed9de
Kartik Krishna Kumbhar <166889480+kartikkumbhar150@users.noreply.github.com>
2026-08-15 13:46:05 +0530

    Solved 76. Minimum Window Substring

---
+++ LeetCode/HashMaps/0076-minimum-window-substring/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        Map<Character, Integer> map1 = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            map1.put(ch, map1.getOrDefault(ch, 0)+1);
+        }
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        int left = 0;
+        int start = 0;
+        int have = 0;
+        int max = Integer.MAX_VALUE;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map2.put(ch, map2.getOrDefault(ch,0)+1);
+
+            if(map1.containsKey(ch) && map1.get(ch).equals(map2.get(ch))){
+                have++;
+            }
+            while(have == map1.size()){
+                if(max > i-left+1){
+                    max = i-left+1;
+                    start = left;
+                }
+                char c = s.charAt(left);
+                map2.put(c, map2.getOrDefault(c,0)-1);
+
+                if(map1.containsKey(c) && map2.get(c) < map1.get(c)){
+                    have--;
+                }
+                left++;
+            }
+        }
+        return max == Integer.MAX_VALUE ? "" : s.substring(start, start+max);
+    }
+}
