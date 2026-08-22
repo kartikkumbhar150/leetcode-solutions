c8a8282ebf840c73173efea042f696b4b26fd443
Kartik Krishna Kumbhar <166889480+kartikkumbhar150@users.noreply.github.com>
2026-08-22 13:23:18 +0530

    Solved 2176. Parallel Courses III

---
+++ LeetCode/Arrays/2176-parallel-courses-iii/Solution.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minimumTime(int n, int[][] relations, int[] time) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[n + 1];
+
+        for (int[] relation : relations) {
+            int u = relation[0];
+            int v = relation[1];
+
+            graph.get(u).add(v);
+            indegree[v]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int[] dp = new int[n + 1];
+
+        // Courses with no prerequisites
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+                dp[i] = time[i - 1];
+            }
+        }
+
+        int answer = 0;
+
+        while (!queue.isEmpty()) {
+
+            int u = queue.poll();
+
+            answer = Math.max(answer, dp[u]);
+
+            for (int v : graph.get(u)) {
+
+                dp[v] = Math.max(
+                    dp[v],
+                    dp[u] + time[v - 1]
+                );
+
+                indegree[v]--;
+
+                if (indegree[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
