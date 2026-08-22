988933ecdaff2c8ca6dcb490088412265c1d9809
Kartik Krishna Kumbhar <166889480+kartikkumbhar150@users.noreply.github.com>
2026-08-22 21:30:51 +0530

    Solved 133. Clone Graph

---
+++ LeetCode/HashMaps/0133-clone-graph/Solution.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+        if(node == null){
+            return null;
+        }
+        Map<Node, Node> map = new HashMap<>();
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.offer(node);
+        map.put(node, new Node(node.val));
+        while(!queue.isEmpty()){
+            Node curr = queue.poll();
+
+            for(Node neigh : curr.neighbors){
+                if(!map.containsKey(neigh)){
+                    map.put(neigh, new Node(neigh.val));
+                    queue.offer(neigh);
+                }
+                map.get(curr).neighbors.add(map.get(neigh));
+                
+            }
+
+        }
+        return map.get(node);
+    }
+}
